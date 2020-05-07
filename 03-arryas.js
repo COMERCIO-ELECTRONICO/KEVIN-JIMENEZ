f64775d165423738e08c2ca6855f601efac06117
kevin jimenez <kivito2009@hotmail.com>
2020-05-07 11:56:39 -0500

    ARRGLO

--- 03-arryas.js
+++ 03-arryas.js
@@ -86,7 +86,7 @@ var operadoresMapyFilter = arreglo2
 arreglo uno, multiplique * 2, y me filtrar los pares, usar map y filter
 arreglo dos, filter los los impares , sumar + 2
 
-var a = {
+{
     arreglos: {
         uno: [2, 4, 5, 8, 6, 9, 10],
         dos: [
