82c54a14ba662d1c69f019b24315d72035581f6a
kevin jimenez <kivito2009@hotmail.com>
2020-05-07 10:53:41 -0500

    funciones

--- 02-funciones.js
+++ 02-funciones.js
@@ -7,7 +7,21 @@ console.log(saludar())
 
 // funciones anonimas
 
-var funcionAnonima = function() {
+var funcionAnonima = function () {
     return 'funcion anonima'
 }
 console.log(funcionAnonima())
+// funciones arrow fat function
+var funcionFechaGorda = (a, b) => {
+    return a + b;
+}
+
+console.log(funcionFechaGorda('a', { a: 1 }));
+
+var json = {
+    suma: (a, b) => {
+        return a + b;
+    }
+}
+console.log(json.suma(1,2));
+
