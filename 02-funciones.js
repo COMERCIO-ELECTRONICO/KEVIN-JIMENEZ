3b7aa820f04f168563be54fcf6a64e3c1c4d7a15
kevin jimenez <kivito2009@hotmail.com>
2020-05-07 10:42:14 -0500

    funcines anonimas

--- 02-funciones.js
+++ 02-funciones.js
@@ -4,3 +4,10 @@ function saludar() {
 
 saludar()
 console.log(saludar())
+
+// funciones anonimas
+
+var funcionAnonima = function() {
+    return 'funcion anonima'
+}
+console.log(funcionAnonima())
