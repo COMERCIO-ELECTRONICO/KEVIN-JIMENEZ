bb16053aada78681e2083c4ef7006a61ce899147
kevin jimenez <kivito2009@hotmail.com>
2020-05-11 10:42:47 -0500

    callbacks

---
+++ callbacks.js
@@ -0,0 +1,37 @@
+// API, rest API
+// Programacion funcional
+
+/*function programacionFuncional(numero1, () => { }, l) {
+
+}*/
+
+function primeraFuncion() {
+    setTimeout(
+        () => {
+            console.log('primera')
+        }, 500
+    )
+
+}
+
+function segundaFuncion() {
+    console.log('2')
+}
+
+//primeraFuncion()
+//segundaFuncion()
+
+function saluda(nombre, callback) {
+    console.log('saludo a', nombre)
+    callback()
+}
+
+function funcionCallback() {
+    primeraFuncion()
+    segundaFuncion()
+    console.log('ya salude')
+}
+
+saluda('Kevin', () => {
+console.log('saludaste')
+})
