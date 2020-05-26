f63072dbe292c73f59681f195821438221dcc450
kevin jimenez <kivito2009@hotmail.com>
2020-05-26 10:41:15 -0500

    rutas lazy

---
+++ ejercicio/src/app/usuario/usuario.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InfoUserComponent } from './info-user/info-user.component';
+import { UsuarioRoutes } from './usuario.routes';
+
+
+
+@NgModule({
+  declarations: [InfoUserComponent],
+  imports: [
+    CommonModule,
+    UsuarioRoutes
+  ]
+})
+export class UsuarioModule { }
