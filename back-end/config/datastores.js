44665c96affbf7b8ee6b69ecc2163204e2eb5bc5
kevin jimenez <kivito2009@hotmail.com>
2020-05-28 11:39:20 -0500

    conection a base de datos

--- back-end/config/datastores.js
+++ back-end/config/datastores.js
@@ -48,8 +48,8 @@ module.exports.datastores = {
     *    (See https://sailsjs.com/config/datastores for help.)                 *
     *                                                                          *
     ***************************************************************************/
-    // adapter: 'sails-mysql',
-    // url: 'mysql://user:password@host:port/database',
+     adapter: 'sails-mysql',
+     url: 'mysql://admin:12345678@localhost:32773/base-sails',
 
   },
 
